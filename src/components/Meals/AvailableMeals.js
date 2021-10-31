b5f9a13a3109c31ebf7d582da27bae5de12c2865
vvduth <ducthai060501@gmail.com>
2021-10-31 12:56:21 +0200

    Update cart item

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -38,6 +38,7 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map(meal => <MealItem key = {meal.id} 
+                                                        ID = {meal.id}
                                                         name = {meal.name}
                                                         description = {meal.description}
                                                         price = {meal.price}/>);
