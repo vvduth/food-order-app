816ed5dc89045daab767c27f0947f0722531b5b9
vvduth <ducthai060501@gmail.com>
2021-10-31 10:54:10 +0200

    Modal state

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,14 +6,14 @@ const Cart = props => {
             {id: 'c1', name: 'Sushi', amount :2 , price: 12.99},
             ].map((item) => <li>{item.name}</li>)}</ul>;
     return (
-        <Modal>
+        <Modal onClose = {props.onClose}>
             {cartItems}
             <div className= {classes.total}>
                 <span>Total Amount</span>
                 <span>35.62</span>
             </div>
             <div className = {classes.actions}>
-                <button className= {classes['button--alt']}>Close</button>
+                <button onClick = {props.onClose} className= {classes['button--alt']}>Close</button>
                 <button className = {classes.button}>Order</button>
             </div>
             
