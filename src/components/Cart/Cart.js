88272cac4ed4f323da63e8fe850b6b974c6253b1
Duc Thai <ducthai060501@gmail.com>
2021-12-20 14:01:43 +0200

    Add order form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,13 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -18,6 +20,10 @@ const Cart = (props) => {
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({...item, amount: 1});
   };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  }
 
   const cartItems = (
     <ul className={classes['cart-items']}>
@@ -34,6 +40,15 @@ const Cart = (props) => {
     </ul>
   );
 
+  const modalAction = <div className={classes.actions}>
+    <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button onClick={orderHandler} className={classes.button}>Order</button>}
+      </div>
+  </div>
+
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -41,12 +56,8 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+      {isCheckout && <Checkout onCancel = {props.onClose} />}
+      {!isCheckout && modalAction}
     </Modal>
   );
 };
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,11 +8,17 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null) ;
 
   useEffect(() => {
     const fetchMeals = async () => {
       
       const response  = await fetch('https://react-http-6e5f7-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+        
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -29,7 +35,10 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals() ;
+    fetchMeals().catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+    });
   },[]);
 
   if (isLoading) {
@@ -37,6 +46,16 @@ const AvailableMeals = () => {
       <section className={classes.MealsLoading}>
         <p>
           Loading ...
+        </p>
+      </section>
+    )
+  };
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>
+          {httpError}
         </p>
       </section>
     )
